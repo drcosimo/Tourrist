a72851b98af56a505de92822fd28045b3759661c
edoardo de parolis <edodeparolis@gmail.com>
2022-02-15 23:21:48 +0100

    scheletro del progetto

---
+++ src/main/java/com/azienda/tourrist/TourristApplication.java
@@ -0,0 +1,13 @@
+package com.azienda.tourrist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourristApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourristApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azienda/tourrist/TourristApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azienda.tourrist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourristApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
